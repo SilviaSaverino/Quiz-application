61d83b3818050d966f97e1ed40f8981504a7b158
Silvia Saverino <silviasave89@gmail.com>
2023-01-06 13:22:17 +0000

    add js content

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('.quiz-form');
+const correctAnswer = ["A", "A", "A", "A", "A"]
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const userAnswer = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
+    
+    let score = 0;
+
+    userAnswer.forEach((answer, index) => {
+        if(answer===correctAnswer[index]){
+            score = score + 1;
+            
+        }
+    });
+
+    console.log(score);
+
+    scrollTo(0, 0);
+    result.classList.remove('hide');
+    result.querySelector('p').textContent=`You scored ${score} /5! `;
+    
+});
+
+
+/*
+access the form
+create access to the user answer and catch them
+check if the correct answer match the user's ones
+increase score if so -> default zero 
+show the result to the user
+remove 'hide' from the result class
+add a scrolltotop(0, 0) when the user click submit
+access result and change the p content of the result using template literals to show the score
+*/
